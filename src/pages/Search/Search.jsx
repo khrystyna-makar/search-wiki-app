0e948dec9dc0943f8ecca5d3946851da9195524d
Khrystyna <khrystyna.makar@gmail.com>
2024-09-14 22:04:36 +0300

    styled search page

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -1,6 +1,28 @@
+import { useSearch } from "../../hooks";
+import { useSearchParams } from "react-router-dom";
+import Container from "../../components/Container";
+import Autocomplete from "../../components/Autocomplete";
+
 const Search = () => {
+
+    const [params, setParams] = useSearchParams();
+    const query = params.get('query');
+
+    const { articles, status } = useSearch(query, 50);
+
     return (
-        <div>Search page</div>
+        <div>
+            <Container>
+                {({ searchValue, onSearchChange, articles }) => <Autocomplete articles={articles} onSearchChange={onSearchChange} searchValue={searchValue} />}
+            </Container>
+            {
+                (!articles.length && status === 'SUCCESS') ? <h3>No results for query: {query}</h3> :
+                    articles.map(article => {
+                        return <div key={article.id}>
+                            <a href={article.id} target="_blank">{article.label}</a>
+                        </div>
+                    })}
+        </div>
     )
 }
 
